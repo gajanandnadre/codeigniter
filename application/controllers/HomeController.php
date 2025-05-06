469310b96a9a0767b8acead5aa1401aebfb6246b
gajanand <nadregaju2019@gmail.com>
2025-05-06 08:19:38 +0530

    dynamic work on banner

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -4,12 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class HomeController extends CI_Controller{
 
     public function index(){
-         
-        // $var['name'] = "Hello codeigniter";
-
-        $data['names'] = array('gaju','rama','maroti','vishnu');
-
-        $this->load->view('about',$data);
+         $this->load->view('front/index');
     }
  
 
---
+++ application/views/front/footer.php
@@ -0,0 +1,17 @@
+  <!-- JS here -->
+      <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets_front/js/vendor/jquery.js"></script>
+      <script src="assets_front/js/vendor/waypoints.js"></script>
+      <script src="assets_front/js/bootstrap-bundle.js"></script>
+      <script src="assets_front/js/meanmenu.js"></script>
+      <script src="assets_front/js/swiper-bundle.js"></script>
+      <script src="assets_front/js/slick.js"></script>
+      <script src="assets_front/js/range-slider.js"></script>
+      <script src="assets_front/js/magnific-popup.js"></script>
+      <script src="assets_front/js/nice-select.js"></script>
+      <script src="assets_front/js/purecounter.js"></script>
+      <script src="assets_front/js/countdown.js"></script>
+      <script src="assets_front/js/wow.js"></script>
+      <script src="assets_front/js/isotope-pkgd.js"></script>
+      <script src="assets_front_front/js/imagesloaded-pkgd.js"></script>
+      <script src="assets_front/js/ajax-form.js"></script>
+      <script src="assets_front/js/main.js"></script>
